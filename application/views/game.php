6bdf6a1fa713bd0af23b211ba379e13251507447
sandro <sandrobasta@outlook.com>
2017-07-27 07:00:15 +0200

    Add stayle, added folder for view, make controller, config

---
+++ application/views/game.php
@@ -0,0 +1,46 @@
+
+<div class="container">
+<div class="col-md-8 ">
+<div class="row">
+	
+    <div class="col-xs-6 col-md-4 sqr bottom right div1"></div>
+	<div class="col-xs-6 col-md-4 sqr bottom right div2"></div>
+	<div class="col-xs-6 col-md-4 sqr bottom div3"></div>
+
+
+</div>
+
+<div class="row">
+	
+	<div class="col-xs-6 col-md-4 sqr bottom right div4"></div>
+	<div class="col-xs-6 col-md-4 sqr bottom right div5"></div>
+	<div class="col-xs-6 col-md-4 sqr bottom div6"></div>
+
+
+</div>
+
+<div class="row">
+	
+	<div class="col-xs-6 col-md-4 sqr right div7"></div>
+	<div class="col-xs-6 col-md-4 sqr right div8"></div>
+	<div class="col-xs-6 col-md-4 sqr div9"></div>
+
+</div>
+
+
+   <button type="button" class="btn btn-success margin">RESET GAME</button>
+
+
+</div>
+
+<div class="col-md-4">
+		
+      <div class="well">
+                    <h4>Side Widget Well</h4>
+                    
+        </div>               
+                  
+ 
+</div>
+</div>
+	
